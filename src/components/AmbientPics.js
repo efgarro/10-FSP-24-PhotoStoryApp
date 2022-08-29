3f8551f8edb27efd8df3179273261f70cacfc086
efgarro <efgarro@yahoo.com>
2022-08-29 15:03:56 -0600

    responsive

--- src/components/AmbientPics.js
+++ src/components/AmbientPics.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Responsive as ResponsiveGridLayout } from "react-grid-layout";
+import { Responsive, WidthProvider } from "react-grid-layout";
+// import { Responsive as ResponsiveGridLayout } from "react-grid-layout";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export class AmbientPics extends React.Component {
   render() {
@@ -11,8 +14,8 @@ export class AmbientPics extends React.Component {
           layouts={this.props.layoutLg}
           rowHeight={63}
           cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-          breakpoints={{ lg: 1150, md: 996, sm: 768, xs: 480, xxs: 0 }}
-          width={1160}
+          breakpoints={{ lg: 992 /*, md: 996, sm: 768, xs: 480, xxs: 0 */}}
+          // width={1200}
           isResizable={true}
           isDraggable={true}
           margin={[25, 25]}
